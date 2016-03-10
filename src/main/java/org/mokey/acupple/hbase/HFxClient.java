66a91396ed838699c9ad47380bdd5e79c7360eb3
enousei <shenhuarj@gmail.com>
2016-03-10 17:50:01 +0800

    add scan

--- src/main/java/org/mokey/acupple/hbase/HFxClient.java
+++ src/main/java/org/mokey/acupple/hbase/HFxClient.java
@@ -51,7 +51,7 @@ public class HFxClient {
         }
     }
 
-    public void creqteTable(Class<? extends HBase> clazz){
+    public void createTable(Class<? extends HBase> clazz){
         TableMeta tableMeta = getTableMeta(clazz);
         try {
             Admin admin = connection.getAdmin();
@@ -97,7 +97,7 @@ public class HFxClient {
         }
     }
 
-    public HBase get(byte[] rowkey, Class<? extends HBase> clazz){
+    public <T extends HBase> T get(byte[] rowkey, Class<? extends HBase> clazz){
         if(rowkey == null)
             return null;
         TableMeta tableMeta = getTableMeta(clazz);
@@ -105,7 +105,7 @@ public class HFxClient {
         try {
             Table table = connection.getTable(tableMeta.getHtableName());
             Result result = table.get(get);
-            return tableMeta.parse(result);
+            return (T)tableMeta.parse(result);
         }catch (Exception ex){
             ex.printStackTrace();
         }
@@ -113,7 +113,7 @@ public class HFxClient {
         return null;
     }
 
-    public List<HBase> search(List<byte[]> rowkeys, FilterBase filter, Class<? extends HBase> clazz){
+    public <T extends HBase>  List<T> search(List<byte[]> rowkeys, FilterBase filter, Class<? extends HBase> clazz){
         TableMeta tableMeta = getTableMeta(clazz);
 
         List<Get> gets = Lists.newArrayList();
@@ -125,14 +125,14 @@ public class HFxClient {
             gets.add(get);
         }
 
-        List<HBase> hBaseList = Lists.newArrayList();
+        List<T> hBaseList = Lists.newArrayList();
         try {
             Table table = connection.getTable(tableMeta.getHtableName());
             Result[] results = table.get(gets);
             for (Result rs: results){
                 HBase hBase = tableMeta.parse(rs);
                 if(hBase != null){
-                    hBaseList.add(hBase);
+                    hBaseList.add((T)hBase);
                 }
             }
         }catch (Exception ex){
@@ -140,6 +140,25 @@ public class HFxClient {
         }
 
         return hBaseList;
+    }
+
+    public <T extends HBase> List<T> scan(byte[] startRow, byte[] stopRow, Class<? extends HBase> clazz){
+        TableMeta tableMeta = getTableMeta(clazz);
+        List<T> list = Lists.newArrayList();
+        try {
+            Table table = connection.getTable(tableMeta.getHtableName());
+            ResultScanner results = table.getScanner(new Scan(startRow, stopRow));
+            for (Result rs: results){
+                T hBase = (T)tableMeta.parse(rs);
+                if(hBase != null){
+                    list.add(hBase);
+                }
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        return list;
     }
 
     public void deleteTable(Class<HBase> clazz){
--- src/test/java/org/mokey/acupple/hbase/Log.java
+++ src/test/java/org/mokey/acupple/hbase/Log.java
@@ -104,18 +104,18 @@ public class Log implements HBase {
 
     public static void main(String[] args){
         HFxClient hFxClient = new HFxClient("127.0.0.1:2181", "/hbase");
-        hFxClient.creqteTable(Log.class);
+        hFxClient.createTable(Log.class);
         Log log = new Log();
         log.setLogId(1);
         log.setEnvGroup("DEV");
         log.setEnv("DEV");
         log.setAppId(9000);
-        log.setMessage("this is a test message");
+        log.setMessage("this is a test message too");
         log.setLogtime(System.currentTimeMillis());
 
         hFxClient.insert(log);
 
-        Log log1 = (Log)hFxClient.get(log.getRowKey(), Log.class);
+        Log log1 = hFxClient.get(log.getRowKey(), Log.class);
 
         System.out.println(log1.getMessage());
     }
